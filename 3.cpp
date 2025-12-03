d67f541249482ffb22fce6a3d07ff5c78ca7d5bb
Areesh Ahmed Khan <k250737@nu.edu.pk>
2025-12-03 23:17:32 +0500

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Product {
+private:
+    string name;
+    int price;
+
+public:
+    void setName(string n) {
+        name = n;
+    }
+
+    void setPrice(int p) {
+        if (p < 0) {
+            cout << "Price cannot be negative!" << endl;
+        } else {
+            price = p;
+        }
+    }
+
+    string getName() { return name; }
+    int getPrice() { return price; }
+};
+
+int main() {
+
+    Product p;
+    p.setName("Laptop");
+    p.setPrice(75000);
+
+    cout << "Product Name: " << p.getName() << endl;
+    cout << "Product Price: " << p.getPrice() << endl;
+
+    return 0;
+}
